44f635982d8002a8df46a41660bcbe7f9cbc9ff9
HATANHack <ebrahimalsmiri12345@gmail.com>
2025-09-24 18:45:09 +0300

    Update and rename fbvid.sh to hatanfbvid.sh

--- .myshfunc/hatanfbvid.sh
+++ .myshfunc/hatanfbvid.sh
@@ -1,35 +1,34 @@
-
-fbvid(){
-# Usage: fbvid "POST_URL"
-if test -d ~/fbvid; then
+hatanfbvid(){
+# Usage: hatanfbvid "POST_URL"
+if test -d ~/hatanfbvid; then
         true
 else
-        mkdir ~/fbvid
+        mkdir ~/hatanfbvid
 fi
-if test -d ~/fbvid/output; then
+if test -d ~/hatanfbvid/output; then
         true
 else
-        mkdir ~/fbvid/output
+        mkdir ~/hatanfbvid/output
 fi
-cd ~/fbvid
+cd ~/hatanfbvid
 url="$1"
 youtube-dl -F ${url}
 read -p "audio format: " fbaudio
 read -p "video format: " fbvideo
 audioext="m4a"
 videoext="mp4"
-echo -e "[fbvid] url: ${url}"
-echo -e "[fbvid] downloading the audio..."
+echo -e "[hatanfbvid] url: ${url}"
+echo -e "[hatanfbvid] downloading the audio..."
 youtube-dl -f ${fbaudio} ${url}
-echo -e "[fbvid] downloading the video..."
+echo -e "[hatanfbvid] downloading the video..."
 youtube-dl -f ${fbvideo} ${url}
 ex=(${url//\// })
 audiofile="$(ls | grep ${audioext})"
 videofile="$(ls | grep ${videoext})"
-echo -e "[fbvid] add the audio to the video..."
+echo -e "[hatanfbvid] add the audio to the video..."
 ffmpeg -i "${videofile}" -i "${audiofile}" -c:v copy -map 0:v:0 -map 1:a:0 -c:a aac -b:a 192k "${ex[4]}.${videoext}"
 rm "${audiofile}"
 rm "${videofile}"
 mv "${ex[4]}.${videoext}" output
-echo -e "[fbvid] done. output: output/${ex[4]}.${videoext}"
+echo -e "[hatanfbvid] done. output: output/${ex[4]}.${videoext}"
 }
